ab93ecd639ba469a351ca50eb4d0fea0f9d8a142
kamellababidi <kamelallababidi@gmail.com>
2017-10-02 13:32:34 +0300

    adding trailer

--- public/components/app.js
+++ public/components/app.js
@@ -1,6 +1,7 @@
 angular.module('myapp',[])
 .component('app',{
-	controller:function(){   
+	controller:function(){  
+		this.trailer=[]; 
 		this.movie=[];
 		this.id=undefined;
 		//change searched movie
@@ -12,21 +13,33 @@ angular.module('myapp',[])
 			for (var i=0;i<window.convertor.length;i++){
 				titel=window.convertor[i].original_title.toLowerCase()
 				if(text==titel){
-					this.id=window.convertor[i].id;
+					x.id=window.convertor[i].id;
 					break;
 				}
 			}
 			console.log(text)
 			 $.ajax({
   		 	  async:false,
-              url: "https://api.themoviedb.org/3/movie/"+this.id+"?api_key=5d2afcf0b3a09621c2e2a3961a8a7024&language=en-US",
+              url: "https://api.themoviedb.org/3/movie/"+x.id+"?api_key=5d2afcf0b3a09621c2e2a3961a8a7024&language=en-US",
               cache: false,
               dataType: 'json',
               success: function(data){
-    			console.log(data)
+              	//
     			x.movie[0]=data
+    			console.log(x.movie)
+    			$.ajax({
+  		 	  async:false,
+              url: "https://api.themoviedb.org/3/movie/"+x.id+"/videos?api_key=5d2afcf0b3a09621c2e2a3961a8a7024&language=en-US",
+              cache: false,
+              dataType: 'json',
+              success: function(data2){
+    			x.trailer[0]=data2
+    			console.log(x.trailer)
             }
             })
+            }
+            })
+			 
 			 
 		}
 	},	
--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 var express=require('express');
-var db = require("./Database/config.js");
-var schema= require("./Database/Model/Movie.js");
+//var db = require("./Database/config.js");
+//var schema= require("./Database/Model/Movie.js");
 var app=express();
 
 //trying the database
 
 app.use(express.static(__dirname));
-//var port = process.env.PORT||8080;
-app.listen(3000,function(err){
+var port = process.env.PORT||3000;
+app.listen(port,function(err){
 	console.log('connected')
 })
 
